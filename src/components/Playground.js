b76fc417b4a56510c62c62f6410fbff4ed223bac
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-26 16:15:51 +0530

    kk

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 const Playground = ({ setIsAuthenticated }) => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    navigate("/"); // 🔑 IMPORTANT CHANGE
-  };
-
   return (
     <div>
       <h3>Hi Welcome to Code PlayGround</h3>
 
-      <button onClick={handleLogout}>
+      <button onClick={() => setIsAuthenticated(false)}>
         Log Out
       </button>
     </div>
